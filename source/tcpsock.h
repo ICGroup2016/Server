99814d85f81f64c103f10ebe92d49a45ec19578a
TanyaAdams1 <yutao@pku.edu.cn>
2016-11-26 12:58:42 +0800

    ty

---
+++ source/tcpsock.h
@@ -0,0 +1,19 @@
+#ifndef TCPSOCK_H
+#define TCPSOCK_H
+#include"stdafx.h"
+class TcpSock
+{
+public:
+    TcpSock(int sockDescript,int id,QString name);
+    void sendMessage(Message msg);
+    Message getMessage() const;
+signals:
+    void emitMessage();
+private:
+    QTcpSocket *socket;
+    Message message;
+    QMutex *readLock;
+    QMutex *writeLock;
+};
+
+#endif // TCPSOCK_H
