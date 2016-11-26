4ab20a82063467042fe020943c113a1c0de8eb49
TanyaAdams1 <yutao@pku.edu.cn>
2016-11-26 12:57:37 +0800

    ty

--- stdafx.h
+++
@@ -1,8 +0,0 @@
-#ifndef STDAFX_H
-#define STDAFX_H
-#include<QtCore>
-#include<QtNetwork/QtNetwork>
-#include<QtGui/QtGui>
-#include"message.h"
-#include"tcpsock.h"
-#endif // STDAFX_H
--- tcpsock.cpp
+++
@@ -1,6 +0,0 @@
-#include "tcpsock.h"
-#include <QtCore>
-TcpSock::TcpSock(int sockDescript, int id, QString name)
-{
-
-}
